623aa24736802d4f58ca82d7881bbd3ad73d5ded
ahmed zribi <zribi4641@gmail.com>
2025-05-01 04:06:22 +0100

    Commit new version

--- models/categorie.js
+++ models/categorie.js
@@ -2,7 +2,6 @@ const mongoose=require('mongoose')
 
 const categorieSchema=mongoose.Schema({
     nom :{type:String,unique: true ,required:true},
-    chambretitle:{type:String ,required:false}, // hetha el attribut maktebtouch fil diagramme de classe 5ater zeyd ena juste bech nesta3mlo fil affichage mte3 les chambre wala nejem n7oto 3adi 5ater ena fil formulaire base taw n7oto ka list fiha les combinaison lkol w howa ya5tar mnha
     description :{type:String,required:true},
     disponible: {type: Boolean, default: true},
     capacite: {type:Number,required:true},
